baae44b57250063e82c9416a5a2cf851b304b145
Vinícius <viniscrv@gmail.com>
2022-12-15 16:15:49 -0300

    criado componente Product

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,10 @@
+import Image from "next/image";
 import { styled } from "../styles";
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import camiseta1 from "../assets/camisetas/1.png"
+import camiseta2 from "../assets/camisetas/2.png"
+import camiseta3 from "../assets/camisetas/3.png"
 
 const Button = styled("button", {
   backgroundColor: "$green300",
@@ -10,9 +16,23 @@ const Button = styled("button", {
 
 export default function Home() {
   return (
-    <>
-      <h1>Home</h1>
-      <Button>Click me</Button>
-    </>
-  );
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt=""/>
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt=""/>
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );  
 }
