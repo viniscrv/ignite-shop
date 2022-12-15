2e9d2ce31cf857c2f1d96f5d381020a8cc70fa50
Vinícius <viniscrv@gmail.com>
2022-12-15 15:03:17 -0300

    configurado biblioteca stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,18 @@
+import { styled } from "../styles"
+
+const Button = styled("button", {
+  backgroundColor: "$purple",
+  borderRadius: 4,
+  border: 0,
+  padding: "4px 8px",
+  cursor: "pointer",
+})
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+      <Button>Click me</Button>
+    </>
   )
 }
