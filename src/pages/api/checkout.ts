bf19d8b09f71e267e1fe4898af57488edc821d8c
Vinícius <viniscrv@gmail.com>
2022-12-17 02:13:29 -0300

    adicionado dados para página de Success

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -4,7 +4,7 @@ import { stripe } from "../../lib/stripe";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
     const { priceId } = req.body;
-    const successUrl = `${process.env.NEXT_URL}/success`;
+    const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
     const cancelUrl = `${process.env.NEXT_URL}/`;
 
     if (req.method !== "POST") {
