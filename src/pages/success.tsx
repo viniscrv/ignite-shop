038a10a891a516fa2247e4405056073ea0b4eb87
Vinícius <viniscrv@gmail.com>
2022-12-17 01:48:38 -0300

    criado página Success

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainter } from "../styles/pages/success";
 
 
 export default function Success() {
     return (
-        <h1>Success</h1>
+        <SuccessContainter>
+            <h1>Compra efetuada</h1>
+
+            <ImageContainer>
+                
+            </ImageContainer>
+
+            <p>Uhu <strong>Vinícius de Carvalho</strong>, sua <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua casa.</p>
+
+            <Link href="/">
+                Voltar ao catálogo
+            </Link>
+        </SuccessContainter>
     )
 }
